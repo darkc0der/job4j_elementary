12b508b99d2dd9ddb07fb70be93e7985b960afcf
PavelAll <alpa2020mi@yandex.ru>
2021-02-25 16:15:09 +0800

    5. Конвертер валюты [#393740]

---
+++ src/ru/job4j/block_one_basic_syntax/converter/Converter.java
@@ -0,0 +1,18 @@
+package ru.job4j.block_one_basic_syntax.converter;
+
+public class Converter {
+    public static double rubleToEuro(double value) {
+        return value / 89;
+    }
+
+    public static double rubleToDollar(double value) {
+        return value / 73;
+    }
+
+    public static void main(String[] args) {
+        double euro = Converter.rubleToEuro(10.0);
+        double dollar = Converter.rubleToDollar(1.0);
+        System.out.println("10 rubles are " + euro + " euro.");
+        System.out.println("1 rubles are " + dollar + " dollar.");
+    }
+}
