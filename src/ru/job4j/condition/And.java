2dd36f46be5750a5ff6885d0594c04886e7bed09
PavelAll <alpa2020mi@yandex.ru>
2021-03-02 01:09:45 +0800

    4.3.1. Оператор &&[#393694]

---
+++ src/ru/job4j/condition/And.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+/* 4.3.1. Оператор && [#393694] */
+public class And {
+    public static void main(String[] args) {
+        boolean first = true;
+        boolean second = true;
+        boolean join = first && second;
+        System.out.println(join);
+
+        first = true;
+        second = false;
+        join = first && second;
+        System.out.println(join);
+
+        first = false;
+        second = true;
+        join = first && second;
+        System.out.println(join);
+
+        first = false;
+        second = false;
+        join = first && second;
+        System.out.println(join);
+    }
+}
