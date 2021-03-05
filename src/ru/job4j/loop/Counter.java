894032380f337e049153499da553bdd8814f8176
PavelAll <alpa2020mi@yandex.ru>
2021-03-06 06:12:58 +0800

    5.1. Подсчет суммы чётных чисел в диапазоне[#393806]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -1,6 +1,17 @@
 package ru.job4j.loop;
 /* 5.0.1. Сумма чисел. [#393803] */
 public class Counter {
+
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for ( ; start <= finish; start++) {
+            if (start % 2 == 0) {
+                sum += start;
+            }
+        }
+        return sum;
+    }
+
     public static int sum(int start, int finish) {
         int sum = 0;
         for (; start <= finish; start++) {
@@ -10,8 +21,12 @@ public class Counter {
     }
 
     public static void main(String[] args) {
-        System.out.println(sum(0, 10));
+        System.out.println(sum(1, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 2));
     }
 }
---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+/* 5.1. Подсчет суммы чётных чисел в диапазоне [#393806] */
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int rsl = Counter.sumByEven(1, 10);
+        int expected = 30;
+        assertThat(rsl, is(expected));
+    }
+}
