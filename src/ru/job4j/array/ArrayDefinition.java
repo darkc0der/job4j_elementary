5fecac5c1b508d495ea72cf64e087bc0749413ec
alpa2020mi@yandex.ru <1983Q1983w%>
2021-03-16 12:33:58 +0800

    6.0.1.1. Размер массива[#393733]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,7 +4,13 @@ public class ArrayDefinition {
     public static void main(String[] args) {
 
         short[] ages = new short[10];
+        System.out.println("Size ages: " + ages.length);
+
         String[] surname = new String[100500];
+        System.out.println("Size surname: " + surname.length);
+
         float[] prices = new float[40];
+        System.out.println("Size prices: " + prices.length);
+
     }
 }
